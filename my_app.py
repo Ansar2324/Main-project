2c9572d53a7125aed834931b5aaa751e15a155c6
Ansar2324 <133509024+Ansar2324@users.noreply.github.com>
2023-05-20 18:18:11 +0600

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,1 +1,11 @@
 # напиши здесь код основного приложения и первого экрана
+
+class MainWin(QWidget):
+  def __init__(self):
+    super().__init__()
+    self.set_appear()
+    self.initUI()
+    self.connects()
+    self.show()
+  def set_appear(self):
+    
